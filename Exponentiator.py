afcdb74539396f3cba724115cca237627a582cc1
Joey8419 <jnelson297@gmail.com>
2023-11-07 16:39:24 -0500

    finished

---
+++ Exponentiator.py
@@ -0,0 +1,19 @@
+## Define a function named *exponentiate* 
+## Takes two integers as arguments.
+## Returns the value of the first integer raised to the power of the second integer.
+
+def exponentiate(val1, val2):
+    return val1 ** val2
+
+## Define a function named *raise_to_fourth_power
+def raise_to_fourth_power(val1):
+    val1**4
+    return exponentiate(val1, 4)
+    
+
+# Define a function named *raise_to_fourth_power
+square = lambda x: exponentiate(x, 2)
+cube = lambda x: exponentiate(x, 3)
+print(square(2))
+print(cube(3))
+print(raise_to_fourth_power(2))
---
+++ greet.py
@@ -0,0 +1,16 @@
+##Define a function called *greet* Takes an argument called *name*.
+
+def greet(name):
+    print ("Hi " + name)
+
+
+## Takes an argument called *name*. that meets the following criteria:
+def name_input():
+    name = input("Provide your name ")  
+    return name
+
+
+greet(name_input())
+
+
+    
